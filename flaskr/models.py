07bc5ed7e757cd086e29e65344aaaddeb1302405
Youngho Cho <dominoyh5@gmail.com>
2021-03-31 19:56:44 +0900

    add Lecture class

--- flaskr/models.py
+++ flaskr/models.py
@@ -7,7 +7,7 @@ class Student(db.Model):
     name = db.Column(db.Text, nullable=False)
     email = db.Column(db.Text, nullable=False)
     password = db.Column(db.DateTime, nullable = False)
-    created = db.Column(db.DateTime,nullable=False,default=datetime.datetime.now)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
   
     def __repr__(self):
         return '<Student %r>' % self.studentId
@@ -18,20 +18,33 @@ class Tutor(db.Model):
     name = db.Column(db.Text, nullable=False)
     email = db.Column(db.Text, nullable=False)
     password = db.Column(db.Text, nullable = False)
-    created = db.Column(db.DateTime,nullable=False,default=datetime.datetime.now)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
    
     def __repr__(self):
         return '<Tutor %r>' % self.tutorId
 
 class Subjects(db.Model):
+    __tablename__ = "subjects"
     subjectId = db.Column(db.Integer, primary_key=True)
     tutorId = db.Column(db.Text, db.ForeignKey('tutor.tutorId'))
     className = db.Column(db.Text)
     countryName = db.Column(db.Text)
     language = db.Column(db.Text)
     duration = db.Column(db.Text)
-    created = db.Column(db.DateTime,nullable=False,default=datetime.datetime.now)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
 
     def __repr__(self):
         return '<Subjects %r>' % self.subjectId
        
+class Lecture(db.Model):
+    __tablename__ = "lecture"
+    lectureId = db.Column(db.Integer, primary_key="True")
+    classId = db.Column(db.Integer, db.ForeignKey('subjects.subjectId'), nullable=False)
+    title = db.Column(db.Text, nullable="False")
+    file = db.Column(db.LargeBinary)
+    contents = db.Column(db.Text)
+    description = db.Column(db.Text, nullable=False)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
+
+    def __repr__(self):
+        return '<Lecture %r>' % self.lectureId
