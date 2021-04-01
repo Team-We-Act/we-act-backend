9a956d7b4ff752d8bed464843a46e7b2d5fcdab0
Youngho Cho <dominoyh5@gmail.com>
2021-04-01 12:55:04 +0900

    add relationship on models.py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -28,6 +28,9 @@ def create_app(test_config=None):
     # ORM
   db.init_app(app)
 
+  with app.app_context():
+    db.create_all()
+
   # from .models import Classes
   # from . import views
   # app.register_blueprint(views.bp)
--- flaskr/models.py
+++ flaskr/models.py
@@ -7,8 +7,9 @@ class Student(db.Model):
     subjectId = db.Column(db.Text, db.ForeignKey('subject.subjectId'))
     name = db.Column(db.Text, nullable=False)
     email = db.Column(db.Text, nullable=False)
-    password = db.Column(db.DateTime, nullable = False)
-    created = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
+    password = db.Column(db.Text, nullable = False)
+    created = db.Column(db.Text, nullable=False, default=datetime.datetime.now)
+    subject = db.relationship("Subject", back_populates="student")
   
     def __repr__(self):
         return '<Student %r>' % self.studentId
@@ -20,6 +21,7 @@ class Tutor(db.Model):
     email = db.Column(db.Text, nullable=False)
     password = db.Column(db.Text, nullable = False)
     created = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
+    subject = db.relationship("Subject", back_populates="tutor")
    
     def __repr__(self):
         return '<Tutor %r>' % self.tutorId
@@ -33,6 +35,9 @@ class Subject(db.Model):
     language = db.Column(db.Text)
     duration = db.Column(db.Text)
     created = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
+    student = db.relationship("Student", back_populates="subject")
+    tutor = db.relationship("Tutor", back_populates="subject")
+    lecture = db.relationship("Lecture", back_populates="subject")
 
     def __repr__(self):
         return '<Subjects %r>' % self.subjectId
@@ -40,12 +45,13 @@ class Subject(db.Model):
 class Lecture(db.Model):
     __tablename__ = "lecture"
     lectureId = db.Column(db.Integer, primary_key="True")
-    classId = db.Column(db.Integer, db.ForeignKey('subjects.subjectId'), nullable=False)
+    subjectId = db.Column(db.Integer, db.ForeignKey('subject.subjectId'), nullable=False)
     title = db.Column(db.Text, nullable="False")
     file = db.Column(db.LargeBinary)
     contents = db.Column(db.Text)
     description = db.Column(db.Text, nullable=False)
     created = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
+    subject = db.relationship("Subject", back_populates="lecture")
 
     def __repr__(self):
         return '<Lecture %r>' % self.lectureId
