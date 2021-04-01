dc649ced2f99881b3aeb9a567ee69b0a5ef507ab
harrywinks <skwent77@gmail.com>
2021-04-01 18:30:40 +0900

    bug: oauth2callback

--- flaskr/user.py
+++ flaskr/user.py
@@ -1,5 +1,5 @@
 from flask import Blueprint,redirect
-from flask import render_template
+from flask import render_template,request,url_for,session
 import google.oauth2.credentials
 import google_auth_oauthlib.flow
 bp=Blueprint('user', __name__, url_prefix='/')
@@ -27,7 +27,7 @@ def authorize():
     flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
         CLIENT_SECRETS_FILE,scopes=SCOPES
     )
-    flow.redirect_uri = flask.url_for('oauth2callback',_external=True)
+    flow.redirect_uri = url_for('oauth2callback',_external=True)
     authorization_url, state = flow.authorization_url(
       # Enable offline access so that you can refresh an access token without
       # re-prompting the user for permission. Recommended for web server apps.
@@ -36,9 +36,9 @@ def authorize():
       include_granted_scopes='true')
 
   # Store the state so the callback can verify the auth server response.
-    flask.session['state'] = state
+    session['state'] = state
 
-    return flask.redirect(authorization_url)
+    return redirect(authorization_url)
 
 
 @bp.route('oauth2callback')
@@ -46,15 +46,17 @@ def oauth2callback():
     state = sessions['state']
 
     flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
-    flow.redirect_uri = flask.url_for('oauth2callback', _external=True)
+    flow.redirect_uri = url_for('oauth2callback', _external=True)
 
     # Use the authorization server's response to fetch the OAuth 2.0 tokens.
-    authorization_response = flask.request.url
+    authorization_response = request.url
     flow.fetch_token(authorization_response=authorization_response)
 
     # Store credentials in the session.
     # ACTION ITEM: In a production app, you likely want to save these
     #              credentials in a persistent database instead.
     credentials = flow.credentials
-    flask.session['credentials'] = credentials_to_dict(credentials)
-    return flask.redirect(flask.url_for('test_api_request'))
+    session['credentials'] = credentials_to_dict(credentials)
+    return redirect(url_for('test_api_request'))
+
+  
